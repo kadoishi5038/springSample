fff52717f58bea7d490bdf4e8e71d5faacdef54f
root <kadoishi5038@gmail.com>
2024-08-30 14:28:02 +0900

    bookinfo作成途中

---
+++ src/main/java/com/example/demo/entity/Bookinfo.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+/**
+ * ユーザー情報 Entity
+ */
+@Entity
+@Data
+@Table(name = "bookinfo")
+public class Bookinfo implements Serializable {
+  /**
+   * ID
+   */
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  /**
+   * isbn
+   */
+  @Column(name = "isbn")
+  private String isbn;
+  /**
+   * タイトル
+   */
+  @Column(name = "title")
+  private String title;
+  /**
+   * 電話番号
+   */
+  @Column(name = "price")
+  private String price;
+}
---
+++ src/main/java/com/example/demo/service/BookinfoService.java
@@ -0,0 +1,76 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entity.Bookinfo;
+import com.example.demo.repository.BookinfoRepository;
+
+/**
+ * ユーザー情報 Service
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class BookinfoService {
+  /**
+   * ユーザー情報 Repository
+   */
+  @Autowired
+  private BookinfoRepository bookinfoRepository;
+
+  /**
+   * ユーザー情報 全検索
+   * @return 検索結果
+   */
+  public List<Bookinfo> searchAll() {
+    return bookinfoRepository.findAll();
+  }
+
+//  /**
+//   * ユーザー情報 新規登録
+//   * @param user ユーザー情報
+//   */
+//  public void create(UserRequest userRequest) {
+//    Date now = new Date();
+//    User user = new User();
+//    user.setName(userRequest.getName());
+//    user.setAddress(userRequest.getAddress());
+//    user.setPhone(userRequest.getPhone());
+//    user.setCreateDate(now);
+//    user.setUpdateDate(now);
+//    userRepository.save(user);
+//  }
+//  
+  /**
+   * ユーザー情報 主キー検索
+   * @return 検索結果
+   */
+  public Bookinfo findById(Long id) {
+    return bookinfoRepository.findById(id).get();
+  }
+//  /**
+//   * ユーザー情報 更新
+//   * @param user ユーザー情報
+//   */
+//  public void update(UserUpdateRequest userUpdateRequest) {
+//    User user = findById(userUpdateRequest.getId());
+//    user.setAddress(userUpdateRequest.getAddress());
+//    user.setName(userUpdateRequest.getName());
+//    user.setPhone(userUpdateRequest.getPhone());
+//    user.setUpdateDate(new Date());
+//    userRepository.save(user);
+//  }
+//  
+//  /**
+//   * ユーザー情報 物理削除
+//   * @param id ユーザーID
+//   */
+//  public void delete(Long id) {
+//    User user = findById(id);
+//    userRepository.delete(user);
+//  }
+
+}
